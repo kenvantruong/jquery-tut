2bbc33968a69a44cd3994c5e54a1efeab453e1db
Ken Truong <kenvantruong@gmail.com>
2018-07-16 23:59:18 -0700

    all

---
+++ js/app.js
@@ -0,0 +1,112 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function highestNumber(array) {
+//     highestNum = 0;
+//     secondHighest = 0;
+//     for (var i=0; i < array.length; i++) {
+//         if (highestNum > array[i]) {
+//             highestNum = array[i]
+//         }
+//     }
+    
+// }
+
+// console.log(highestNumber([55,500,700,999])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var sonLuChallenege = [1,56,88,95,23];
+// var highestNumber = 0;
+
+// for (var i=0; i < sonLuChallenege.length;i++) {
+//     if (sonLuChallenege[i] > highestNumber) {
+//         highestNumber = sonLuChallenege[i];
+//     }
+// }
+// console.log(highestNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function largestOfFour(arr) {
+//     var largestNumbers = [];
+//     var currentLargest;
+//     for (var x=0; x < arr.length; x++) {
+//         currentLargest = 0;
+//         for (var y=0; y < arr[x].length; y++) {
+//             if (arr[x][y] > currentLargest) {
+//                 currentLargest = arr[x][y];
+//             }
+//         }
+//         largestNumbers.push(currentLargest);
+//     }
+//     return largestNumbers;
+// }
+  
+// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
